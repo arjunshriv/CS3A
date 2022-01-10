e775810e9fcb666fe63f369b76519ce58384a595
Arjun Shrivastava <ArjunShrivastava@Arpitas-MacBook-Pro.local>
2022-01-09 17:50:44 -0800

    Week1 1

---
+++ Week1/AssignmentOne.py
@@ -0,0 +1,20 @@
+"""
+    Assignment 1: Opening Lines
+    Submitted by Arjun Shrivastava
+    Submitted:  January 9, 2022
+    This program demonstrates printing lines of text to the screen
+"""
+
+def print_header():
+    """Print a project message."""
+    print("STEM Center Temperature Project")
+    print("Arjun Shrivastava")
+
+if __name__ == "__main__":
+    print_header()
+
+"""
+Arpitas-MacBook-Pro:Assignments ArjunShrivastava$ python Week1/AssignmentOne.py 
+STEM Center Temperature Project
+Arjun Shrivastava
+"""
